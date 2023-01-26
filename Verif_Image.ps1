37eca5260367ec8f40f28d2688cb8f790fb96a68
ikhela-04KK <kankoffi36@gmail.com>
2023-01-26 15:01:30 +0100

    Mise en place de nouvelle fonctionnalité

---
+++ Verif_Image.ps1
@@ -0,0 +1,14 @@
+#?PREMIERE ETATPE 
+
+# * Ecriture d'un script qui permet de verifier si un Dossier contient ou pas des images 
+# * s'il contient des images --- Nom dossier : contient des images 
+# * s'il ne contient pas d'image affche ce dossier ne contient pas contient pas d'iamge 
+# * à la fin il montre tous les dossiers qui ne contiennent pas des images 
+
+#?DEUXIEME ETAPE 
+
+# * S'il contien des images en un format différents de jpeg , il faut le convertir 
+# * N'oublié pas aussi de supprimer le fichier qui a été convertir au prealable 
+
+#?TROISIEME ETAPE 
+#* Mettre ton code sur GitHub en ligne de commande 
