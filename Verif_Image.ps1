0d9a4552d1cd0d99ce554e659e386e0d31970a60
ikhela-04KK <kankoffi36@gmail.com>
2023-01-26 21:33:03 +0100

    Mise en place de nouvelle fonctionnalité

---
+++ Verif_Image.ps1
@@ -0,0 +1,10 @@
+#? PREMIRE PARTIE 
+    #* Parcourir les dossiers et verifer s'il y'a des images  
+    #* s'il n' y a pas d'image il faut afficher le dossier le lien du fichier ou il n'y a pas d'image 
+
+#? DEUXIEME PARTIE 
+    #* S'il n' y des images qui ne sont pas dans un format jpeg , il faut que tu les convertissent 
+    #* s'il y ' a une image au format specifier , il faut que tu passe 
+
+#? TROISIEME PARTIE
+    #* 
